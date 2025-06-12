1d7971fab5abdfd5bfbe4e1a32822b924559ff75
szczepciak14 <szczepan.gwiazda@wp.pl>
2025-06-12 19:55:34 +0200

    BogdanBoner 2.0

---
+++ Assets/Scripts/AlienLeaderScript.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienLeaderScript : MonoBehaviour
+{
+    public GameObject alienBullet;
+    public float speed = 5.0f;
+    public float borderValue = 8f;
+
+    private Vector3 target;
+    private Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector3 (borderValue, transform.position.y, transform.position.z);
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var step = speed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        if(Vector3.Distance(transform.position, target) < 0.001f)
+        {
+            target = new Vector3(target.x * -1, transform.position.y, transform.position.z);
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 pos = transform.position - new Vector3(0, 1, 0);
+
+        Instantiate(alienBullet, pos, Quaternion.identity);
+    }
+
+    void ToggleVisible()
+    {
+        rend.enabled = !rend.enabled;
+    }
+
+    public void StartLeader()
+    {
+        Invoke("ToggleVisible", 6);
+        InvokeRepeating("Shoot", 6, 2);
+    }
+
+    public void StopLeader()
+    {
+        CancelInvoke("Shoot");
+    }
+}
--- Assets/Scripts/FleetScript.cs
+++ Assets/Scripts/FleetScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class FleetScript : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class FleetScript : MonoBehaviour
     public float moveTime = 2f;
     public float moveSpeed = 0.3f;
     public bool gameRun;
+    public TMP_Text endText;
 
     private int moveCounter = 0;
     private int moveDirection;
@@ -25,30 +28,40 @@ public class FleetScript : MonoBehaviour
         {
             StopGame(true);
         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     public void StopGame(bool win)
     {
         CancelInvoke("Move");
         gameRun = false;
+
+        endText.gameObject.SetActive(true);
+        endText.text = (win) ? "LGBTQ DEFEATED" : "You Lose !!!";
     }
 
     public void StartGame()
     {
         InvokeRepeating("Move", moveTime, moveTime);
         gameRun = true;
+
+        endText.gameObject.SetActive(false);
     }
 
     void Move()
     {
-        transform.position += new Vector3(moveSpeed + moveDirection, 0f, 0f);
+        transform.position += new Vector3(moveSpeed * moveDirection, 0f, 0f);
 
         moveCounter += moveDirection;
 
         if (moveCounter <= -4 || moveCounter >= 4)
         {
             moveDirection *= -1;
-            transform.position -= new Vector3(0, moveSpeed, 0);
+            transform.position -= new Vector3(0, moveSpeed);
         }
     }
 }
